c394ac8a93398e4a2b57a3d7e4bb8e76d3e4cef4
Marta Molina <marta.molina@live.u-tad.com>
2021-11-07 19:58:03 +0100

    Toca crear añadir elementos al layout de la vista.

--- app/src/main/java/com/marta/ud5_02_recyclerview_martamolina/RepositoryAdapter.kt
+++ app/src/main/java/com/marta/ud5_02_recyclerview_martamolina/RepositoryAdapter.kt
@@ -1,23 +1,27 @@
 package com.marta.ud5_02_recyclerview_martamolina
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.marta.ud5_02_recyclerview_martamolina.databinding.ActivityMainBinding
 import com.marta.ud5_02_recyclerview_martamolina.databinding.ItemRepositoryBinding
 import com.marta.ud5_02_recyclerview_martamolina.model.Repository
 
-class RepositoryAdapter(repositories: MutableList<Repository>): RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
+class RepositoryAdapter( private val repositories: MutableList<Repository>): RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemRepositoryBinding.inflate(layoutInflater,parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val repository = repositories[position]
+        //TODO: añadir los param de los elementos de la vista
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return repositories.size
     }
 
     inner class ViewHolder(val binding: ItemRepositoryBinding) : RecyclerView.ViewHolder(binding.root)
