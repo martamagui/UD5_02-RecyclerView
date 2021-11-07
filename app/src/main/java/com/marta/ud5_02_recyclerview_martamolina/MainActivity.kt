7b323c77ba3cd92cd2bcb6ac0eb3291c18877391
Marta Molina <marta.molina@live.u-tad.com>
2021-11-07 19:43:14 +0100

    Está recogiendo los 21 repos, nice.

--- app/src/main/java/com/marta/ud5_02_recyclerview_martamolina/MainActivity.kt
+++ app/src/main/java/com/marta/ud5_02_recyclerview_martamolina/MainActivity.kt
@@ -2,7 +2,11 @@ package com.marta.ud5_02_recyclerview_martamolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
 import com.marta.ud5_02_recyclerview_martamolina.databinding.ActivityMainBinding
+import com.marta.ud5_02_recyclerview_martamolina.model.RepositoryResponse
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.rvRepositories.layoutManager = LinearLayoutManager(this)
+        val app = application as App
+        val repositories = app.repositoriesList
+        val repositoriesResults = Gson().fromJson(RecyclerFakeData.repositoriesJson, RepositoryResponse::class.java)
+        Log.d("mainMsg",(repositoriesResults.size).toString())
 
     }
 }
