7e72899ea0d064c9d8643c9daf56cdcbbcf8ca51
Marta Molina <marta.molina@live.u-tad.com>
2021-11-07 19:05:07 +0100

    De RepositoryResponse a Repository

--- app/src/main/java/com/marta/ud5_02_recyclerview_martamolina/model/Repository.kt
+++ app/src/main/java/com/marta/ud5_02_recyclerview_martamolina/model/Repository.kt
@@ -3,12 +3,12 @@ package com.marta.ud5_02_recyclerview_martamolina.model
 data class Repository(
     val name: String,
     val description: String,
-    val ownerAvatarUrl : String,
+    val ownerAvatarUrl: String,
     val ownerName: String,
     val licenseName: String,
     val topics: List<String>,
     val language: String
-){
+) {
     constructor(repositoryResponse: RepositoryResponse) : this(
         repositoryResponse.name,
         repositoryResponse.description,
@@ -17,5 +17,19 @@ data class Repository(
         repositoryResponse.license.licenseName,
         repositoryResponse.topics,
         repositoryResponse.language
-        )
+    )
 }
+
+fun RepositoryResponse.toRepository(): Repository {
+    return Repository(
+        this.name,
+        this.description,
+        this.owner.ownerAvatarUrl,
+        this.owner.ownerName,
+        this.license.licenseName,
+        this.topics,
+        this.language
+    )
+}
+
+
